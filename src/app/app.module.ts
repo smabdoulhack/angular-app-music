7a4ffe677c5bf3d1e60d423d807fa6a364ec5a26
Abdoul Hack <smabdoulhack@outloock.com>
2024-08-12 18:15:45 +0100

    reste le progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+import { AlbumListComponent } from './components/home/album-list/album-list.component';
+import { AlbumComponent } from './components/home/album/album.component';
+import { AsideComponent } from './components/home/aside/aside.component';
+import { AlbumDetailsComponent } from './components/home/album-details/album-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProgressBarComponent } from './components/home/progress-bar/progress-bar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    AlbumListComponent,
+    AlbumComponent,
+    AsideComponent,
+    AlbumDetailsComponent,
+    HeaderComponent,
+    SignUpComponent,
+    PageNotFoundComponent,
+    ProgressBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/home/album-list/album-list.component.ts
@@ -0,0 +1,81 @@
+import { Component, Output } from '@angular/core';
+import { AlbumsService } from '../../../services/albums/albums.service';
+import { Album } from '../../../album';
+
+@Component({
+  selector: 'app-album-list',
+  templateUrl: './album-list.component.html',
+  styleUrl: './album-list.component.css',
+})
+export class AlbumListComponent {
+  albums!: Album[];
+  albumPlayingData: Album | null = null;
+  keyword: string = '';
+  isPlaying: boolean = false;
+
+  constructor(private albumsService: AlbumsService) {}
+
+  ngOnInit() {
+    this.albums = this.albumsService.getAlbums();
+  }
+
+  receivAlbumData(value: Album): void {
+    this.albumPlayingData = value;
+  }
+
+  search(): void {
+    if (this.keyword.trim() !== '') {
+      this.albums = this.albumsService
+        .getAlbums()
+        .filter(
+          (el) =>
+            el.title.toLowerCase().includes(this.keyword.toLowerCase()) ||
+            el.description.toLowerCase().includes(this.keyword.toLowerCase()) ||
+            el.genre.toLowerCase().includes(this.keyword.toLowerCase()) ||
+            el.label.toLowerCase().includes(this.keyword.toLowerCase()) ||
+            el.artist.toLowerCase().includes(this.keyword.toLowerCase())
+        );
+    } else {
+      this.albums = this.albumsService.getAlbums();
+    }
+  }
+
+  receivPlaySignal(value: boolean): void {
+    this.isPlaying = value;
+  }
+
+  // play = async () => {
+  //   let totalstep = 0;
+  //   for (const track of trackList) {
+  //     currentTrackData.value.title = track.title;
+
+  //     console.log('title', currentTrackData.value.title);
+
+  //     currentTrackData.value.number++;
+
+  //     const totalstep = (track.trackDuration * 100) / totalDuration.value;
+
+  //     console.log('totalstep', totalstep);
+
+  //     const step = totalstep / track.trackDuration;
+
+  //     console.log('step', step);
+
+  //     const interval = track.trackDuration * 1000; // Interval en millisecondes (1 seconde)
+
+  //     console.log('interval', interval);
+
+  //     // Attendre chaque intervalle
+  //     const current = await new Promise((resolve) => {
+  //       const intervalId = setInterval(() => {
+  //         progress.value += step;
+  //         if (progress.value >= 1) {
+  //           clearInterval(intervalId);
+  //           resolve();
+  //         }
+  //       }, interval);
+  //     });
+  //   }
+  //   emit('stopPlay', false);
+  // };
+}
